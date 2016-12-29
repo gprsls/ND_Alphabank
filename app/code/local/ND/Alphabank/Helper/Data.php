b576a1afde2fabb3c5c805922719d44c1e35660d
gprsls <gprasoulas@gmail.com>
2016-12-29 16:52:51 +0200

    Update Data.php

--- app/code/local/ND/Alphabank/Helper/Data.php
+++ app/code/local/ND/Alphabank/Helper/Data.php
@@ -2,4 +2,15 @@
 
 class ND_Alphabank_Helper_Data extends Mage_Core_Helper_Abstract
 {
+    public function getInstallments()
+    {
+		$installments = Mage::getStoreConfig('payment/alphabank_checkout/installments',Mage::app()->getStore());          
+        return $installments;
+    }
+	
+	public function getOrderTotal()
+    {
+		$orderTotal = Mage::getSingleton('checkout/session')->getQuote()->getGrandTotal();         
+        return $orderTotal;
+    }
 }
