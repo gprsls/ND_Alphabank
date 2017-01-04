31421f668dbd0c389805d4603a5125d2b1834455
gprsls <gprasoulas@gmail.com>
2017-01-04 13:37:06 +0200

    Transfered code from form.phtml
    Added getMaxInstallments() and getInstallmentsOptions()

--- app/code/local/ND/Alphabank/Block/Checkout/Form.php
+++ app/code/local/ND/Alphabank/Block/Checkout/Form.php
@@ -29,4 +29,38 @@ class ND_Alphabank_Block_Checkout_Form extends Mage_Payment_Block_Form
         }
         return $types;*/
     }
+    public function getMaxInstallments(){
+        $installments = Mage::helper('alphabank/data')->getInstallments();
+        $orderTotal = Mage::helper('alphabank/data')->getOrderTotal();
+        function string_to_array($string) {
+            $exploded_string = explode(",",$string);    // explode with ,
+            $installments_array = array();
+            foreach ($exploded_string as $value) {
+                $result = explode(':',$value);  // explode with :
+                $installments_array[$result[0]] = $result[1];
+            }
+            return $installments_array;
+        }
+        $installments = string_to_array($installments);
+        $max_installments = "";
+        foreach($installments as $threshold => $threshold_value) {  
+            if($orderTotal >= $threshold) {
+                $max_installments = $threshold_value;
+            }
+            if($max_installments > 12) {
+                $max_installments = 12;
+            }
+        }
+        return $max_installments;
+    }
+    public function getInstallmentsOptions($max_installments){
+        $i = 1;
+        $installments_options = array();
+        while($i <= $max_installments){
+            $installments_options[] = $i;
+            $i++;
+        }
+
+        return $installments_options;
+    }
 }
